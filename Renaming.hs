c69450d2879922091144f800a183eecaba3c2a9b
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-09-07 15:58:56 -0400

    remove useless In/Out types

--- Renaming.hs
+++ Renaming.hs
@@ -13,10 +13,7 @@ import Utilities
 import qualified Data.Map as M
 import Data.Traversable (mapAccumL)
 
-type In a = a
-type Out a = a
-
-newtype Renaming = Renaming { unRenaming :: M.Map (In Name) (Out Name) }
+newtype Renaming = Renaming { unRenaming :: M.Map Name Name }
   deriving (Show, NFData)
 
 instance Pretty Renaming where
@@ -33,30 +30,30 @@ mkRenaming = Renaming . M.fromList
 mkIdentityRenaming :: [Name] -> Renaming
 mkIdentityRenaming = mkRenaming . map (id &&& id)
 
-insertRenaming :: In Name -> Out Name -> Renaming -> Renaming
+insertRenaming :: Name -> Name -> Renaming -> Renaming
 insertRenaming n n' = Renaming . M.insert n n' . unRenaming
 
-insertRenamings :: [(In Name, Out Name)] -> Renaming -> Renaming
+insertRenamings :: [(Name, Name)] -> Renaming -> Renaming
 insertRenamings = flip $ foldr (uncurry insertRenaming)
 
-rename :: Renaming -> In Name -> Out Name
+rename :: Renaming -> Name -> Name
 rename = safeRename' Nothing
 
-safeRename :: String -> Renaming -> In Name -> Out Name
+safeRename :: String -> Renaming -> Name -> Name
 safeRename = safeRename' . Just
 
-safeRename' :: Maybe String -> Renaming -> In Name -> Out Name
+safeRename' :: Maybe String -> Renaming -> Name -> Name
 safeRename' mb_stk rn n | Just n' <- rename_maybe rn n = n'
                         | otherwise                    = error $ show (text "Name" <+> pPrint n <+> text "out of scope" <+> maybe empty (\stk -> text "in" <+> text stk) mb_stk <> text "! Renaming:" $$ pPrint rn)
 
-rename_maybe :: Renaming -> In Name -> Maybe (Out Name)
+rename_maybe :: Renaming -> Name -> Maybe Name
 rename_maybe rn n = M.lookup n (unRenaming rn)
 
-renameBinder :: IdSupply -> Renaming -> In Name -> (IdSupply, Renaming, Out Name)
+renameBinder :: IdSupply -> Renaming -> Name -> (IdSupply, Renaming, Name)
 renameBinder ids rn n = (ids', insertRenaming n n' rn, n')
   where (ids', n') = freshName ids (name_string n)
 
-renameBinders :: IdSupply -> Renaming -> [In Name] -> (IdSupply, Renaming, [Out Name])
+renameBinders :: IdSupply -> Renaming -> [Name] -> (IdSupply, Renaming, [Name])
 renameBinders ids rn =
     reassociate . mapAccumL ((associate .) . uncurry renameBinder) (ids, rn)
   where associate   (ids, rn, n)    = ((ids, rn), n)
